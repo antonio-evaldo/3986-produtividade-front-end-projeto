e8d3f3ebbcb866834302f237d880801639da857c
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-07-08 17:56:38 -0300

    Adicionando JS do GPT

--- js/botaoModoEscuro.js
+++ js/botaoModoEscuro.js
@@ -1,6 +1,16 @@
-const botaoModoEscuro = document.querySelector(".cabecalho__switch-input");
+const checkboxModoEscuro = document.getElementById('checkbox-modo-escuro');
 
-botaoModoEscuro.addEventListener("change", () => {
-  const root = document.querySelector(":root");
-  root.classList.toggle("modo-escuro", botaoModoEscuro.checked);
+checkboxModoEscuro.addEventListener('change', () => {
+  if (checkboxModoEscuro.checked) {
+    document.body.classList.add('modo-escuro');
+  } else {
+    document.body.classList.remove('modo-escuro');
+  }
 });
+
+// const botaoModoEscuro = document.querySelector(".cabecalho__switch-input");
+
+// botaoModoEscuro.addEventListener("change", () => {
+//   const root = document.querySelector(":root");
+//   root.classList.toggle("modo-escuro", botaoModoEscuro.checked);
+// });
