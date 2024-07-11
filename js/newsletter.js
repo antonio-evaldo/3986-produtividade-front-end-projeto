5bb4a42b577cf5eadcd32bed43f185c4ef826a8a
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-07-11 15:42:29 -0300

    Checando campo de email com regex

---
+++ js/newsletter.js
@@ -0,0 +1,14 @@
+document.querySelector('.newsletter').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const email = document.querySelector('.newsletter__input').value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(email)) {
+    alert('Email válido! Formulário enviado.');
+    // Aqui você pode enviar o formulário ou realizar outra ação
+    // this.submit(); // Para enviar o formulário
+  } else {
+    alert('Por favor, insira um email válido.');
+  }
+});
--- script.js
+++ script.js
@@ -3,3 +3,4 @@ import "./js/botaoModoEscuro.js";
 import "./js/formPesquisa.js";
 import "./js/botaoExpandirCanais.js";
 import "./js/botoesCategorias.js";
+import "./js/newsletter.js";
